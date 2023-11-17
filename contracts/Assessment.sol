616c8aac11cc6ee0de4cae2177fbc06ebd8fdbea
201911104 <201911104@fit.edu.ph>
2023-11-17 23:07:15 +0800

    other files added

---
+++ contracts/Assessment.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract Assessment {
+    // Owner of the contract
+    address payable public owner;
+
+    // Current balance of the contract
+    uint256 public balance;
+
+    // Event emitted when a deposit occurs
+    event Deposit(uint256 amount);
+
+    // Event emitted when a withdrawal occurs
+    event Withdraw(uint256 amount);
+
+    // Struct to represent a transaction
+    struct Transaction {
+        string transactionType; // "Deposit" or "Withdraw"
+        uint256 amount;         // Amount involved in the transaction
+        uint256 timestamp;      // Timestamp of the transaction
+    }
+
+    // Array to store transaction history
+    Transaction[] public transactionHistory;
+
+    // Constructor to initialize the contract with an initial balance
+    constructor(uint initBalance) payable {
+        // Set the owner to the address that deployed the contract
+        owner = payable(msg.sender);
+
+        // Set the initial balance
+        balance = initBalance;
+    }
+
+    // Function to get the current balance
+    function getBalance() public view returns (uint256) {
+        return balance;
+    }
+
+    // Function to deposit funds into the contract
+    function deposit(uint256 _amount) public payable {
+        // Ensure that the caller is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // Perform the transaction
+        balance += _amount;
+
+        // Record the transaction in history
+        transactionHistory.push(
+            Transaction({
+                transactionType: "Deposit",
+                amount: _amount,
+                timestamp: now
+            })
+        );
+
+        // Emit the Deposit event
+        emit Deposit(_amount);
+    }
+
+    // Custom error for insufficient balance during withdrawal
+    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+
+    // Function to withdraw funds from the contract
+    function withdraw(uint256 _withdrawAmount) public {
+        // Ensure that the caller is the owner
+        require(msg.sender == owner, "You are not the owner of this account");
+
+        // Check if the balance is sufficient for withdrawal
+        if (balance < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        // Perform the transaction
+        balance -= _withdrawAmount;
+
+        // Record the transaction in history
+        transactionHistory.push(
+            Transaction({
+                transactionType: "Withdraw",
+                amount: _withdrawAmount,
+                timestamp: now
+            })
+        );
+
+        // Emit the Withdraw event
+        emit Withdraw(_withdrawAmount);
+    }
+
+    // Function to get the count of transactions in the history
+    function getTransactionHistoryCount() public view returns (uint256) {
+        return transactionHistory.length;
+    }
+
+    // Function to get details of a specific transaction in the history
+    function getTransaction(uint256 index) public view returns (string memory transactionType, uint256 amount, uint256 timestamp ) {
+        // Ensure the index is within the bounds of the transaction history
+        require(index < transactionHistory.length, "Invalid transaction index");
+
+        // Retrieve the transaction at the specified index
+        Transaction storage transaction = transactionHistory[index];
+
+        // Return the details of the transaction
+        return (
+            transaction.transactionType,
+            transaction.amount,
+            transaction.timestamp
+        );
+    }
+}
+
---
+++ pages/index.js
@@ -0,0 +1,211 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+
+export default function HomePage() {
+  const [ethWallet, setEthWallet] = useState(undefined);
+  const [account, setAccount] = useState(undefined);
+  const [atm, setATM] = useState(undefined);
+  const [balance, setBalance] = useState(undefined);
+  const [transactionHistory, setTransactionHistory] = useState([]);
+  const [gasPrice, setGasPrice] = useState(undefined);
+
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const atmABI = atm_abi.abi;
+
+  // Function to initialize the wallet
+  const getWallet = async () => {
+    if (window.ethereum) {
+      setEthWallet(window.ethereum);
+    }
+
+    if (ethWallet) {
+      const accounts = await ethWallet.request({ method: "eth_accounts" });
+      handleAccount(accounts);
+    }
+  };
+
+  // Function to handle the connected account
+  const handleAccount = (accounts) => {
+    const currentAccount = accounts[0];
+    if (currentAccount) {
+      console.log("Account connected: ", currentAccount);
+      setAccount(currentAccount);
+    } else {
+      console.log("No account found");
+    }
+  };
+
+  // Function to connect the MetaMask account
+  const connectAccount = async () => {
+    try {
+      if (!ethWallet) {
+        alert("MetaMask wallet is required to connect");
+        return;
+      }
+
+      const accounts = await ethWallet.request({ method: "eth_requestAccounts" });
+      handleAccount(accounts);
+
+      // Once the wallet is set, get a reference to the deployed contract
+      getATMContract();
+    } catch (error) {
+      console.error("Error connecting account:", error);
+    }
+  };
+
+  // Function to get a reference to the deployed contract
+  const getATMContract = () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(ethWallet);
+      const signer = provider.getSigner();
+      const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
+
+      setATM(atmContract);
+    } catch (error) {
+      console.error("Error getting ATM contract:", error);
+    }
+  };
+
+  // Function to get the current balance
+  const getBalance = async () => {
+    try {
+      if (atm) {
+        const balance = await atm.getBalance();
+        setBalance(balance.toNumber());
+      }
+    } catch (error) {
+      console.error("Error getting balance:", error);
+    }
+  };
+
+  // Function to handle a deposit transaction
+   const deposit = async (amount) => {
+    try {
+      if (atm) {
+        // Validate the amount (you can add additional validation logic)
+        if (amount <= 0) {
+          console.error("Invalid deposit amount");
+          return;
+        }
+
+        const gasPrice = await ethWallet.request({ method: "eth_gasPrice" });
+
+        // Set the gas price in Gwei
+        setGasPrice(ethers.utils.formatUnits(gasPrice, "gwei"));
+
+        const tx = await atm.deposit(amount, {
+          gasPrice: gasPrice,
+        });
+
+        await tx.wait();
+        getBalance();
+        updateTransactionHistory("Deposit", amount);
+      }
+    } catch (error) {
+      console.error("Error depositing:", error);
+    }
+  };
+
+  // Function to handle a withdrawal transaction with a variable amount
+  const withdraw = async (amount) => {
+    try {
+      if (atm) {
+        // Validate the amount (you can add additional validation logic)
+        if (amount <= 0) {
+          console.error("Invalid withdrawal amount");
+          return;
+        }
+
+        const gasPrice = await ethWallet.request({ method: "eth_gasPrice" });
+
+        // Set the gas price in Gwei
+        setGasPrice(ethers.utils.formatUnits(gasPrice, "gwei"));
+
+        const tx = await atm.withdraw(amount, {
+          gasPrice: gasPrice,
+        });
+
+        await tx.wait();
+        getBalance();
+        updateTransactionHistory("Withdraw", amount);
+      }
+    } catch (error) {
+      console.error("Error withdrawing:", error);
+    }
+  };
+
+  // Function to update the transaction history
+  const updateTransactionHistory = (type, amount) => {
+    setTransactionHistory([
+      ...transactionHistory,
+      {
+        type: type,
+        amount: amount,
+        timestamp: new Date().toLocaleString(),
+      },
+    ]);
+  };
+
+  // Function to initialize the user interface
+  const initUser = () => {
+    // Check if the user has MetaMask
+    if (!ethWallet) {
+      return <p>Please install MetaMask to use this ATM.</p>;
+    }
+
+    // Check if the user is connected. If not, connect to their account
+    if (!account) {
+      return (
+        <button onClick={connectAccount}>
+          Please connect your MetaMask wallet
+        </button>
+      );
+    }
+
+    if (balance === undefined) {
+      getBalance();
+    }
+
+    return (
+      <div>
+        <p>Your Account: {account}</p>
+        <p>Your Balance: {balance} ETH</p>
+        <button onClick={deposit}>Deposit 1 ETH</button>
+        <button onClick={withdraw}>Withdraw 1 ETH</button>
+         <button onClick={() => deposit(5)}>Deposit 5 ETH</button>
+        <button onClick={() => withdraw(5)}>Withdraw 5 ETH</button>
+        <p>Gas Price: {gasPrice} Gwei</p>
+        <p>Your Transaction History:</p>
+        <ul>
+          {transactionHistory.map((transaction, index) => (
+            <li key={index}>
+              {transaction.type} - {transaction.amount} ETH -{" "}
+              {transaction.timestamp}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getWallet();
+  }, []);
+
+  return (
+    <main className="container">
+      <header>
+        <h1>Welcome to the Metacrafters ATM!</h1>
+      </header>
+      {initUser()}
+      <style jsx>{`
+        .container {
+          text-align: center;
+        }
+      `}</style>
+    </main>
+  );
+}
+
+
